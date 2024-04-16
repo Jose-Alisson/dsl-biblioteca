3f20e773a9d090dba5c682b8ca24d7bc3c83013f
Jose Alisson <alissonbarbosa.9982@gmail.com>
2024-04-16 19:50:48 -0300

    biblioteca

--- src/app/shared/services/API_URL.ts
+++ src/app/shared/services/API_URL.ts
@@ -1,6 +1,8 @@
 
 
-let isBuild = process.env['IsBUILD'] ?? false
+// let isBuild = process.env['IsBUILD'] ?? false
 
-//export const DSL_API_URL = "https://dsl-biblioteca-api.up.railway.app/api"
-export const DSL_API_URL = isBuild ? "https://dsl-biblioteca-api.up.railway.app/api" : "http://localhost:8080/api"
+// //export const DSL_API_URL = "https://dsl-biblioteca-api.up.railway.app/api"
+// export const DSL_API_URL = isBuild ? "https://dsl-biblioteca-api.up.railway.app/api" : "http://localhost:8080/api"
+
+export const DSL_API_URL = "https://dsl-biblioteca-api.up.railway.app/api"
